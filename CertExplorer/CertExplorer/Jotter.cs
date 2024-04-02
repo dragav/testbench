12b67884a9944ce0e72a3f58693b4ae5a3978815
Dragos Avadanei <dragosav@microsoft.com>
2024-04-02 11:20:17 -0700

    cleanup

--- CertExplorer/CertExplorer/Jotter.cs
+++ CertExplorer/CertExplorer/Jotter.cs
@@ -9,10 +9,38 @@ namespace CertExplorer
     public class Jotter
     {
         public string Token { get; set; }
-        public static void Validate(string token)
+        public static void Validate(string token, SecurityToken jwtToken)
         {
-            var tokenParams = new TokenValidationParameters();
-            var claims = new JwtSecurityTokenHandler().ValidateToken(token, tokenParams, out var jwtToken);
+
+            var issuers = new string[]
+            {
+                "https://login.microsoftonline.com/72f988bf-86f1-41af-91ab-2d7cd011db47",
+                "https://login.microsoftonline.com/72f988bf-86f1-41af-91ab-2d7cd011db47/v2.0/",
+                "https://sts.windows.net/72f988bf-86f1-41af-91ab-2d7cd011db47",
+                "https://sts.windows.net/72f988bf-86f1-41af-91ab-2d7cd011db47/v2.0/"
+            };
+
+            SecurityToken jwtoken;
+            var tokenParams = new TokenValidationParameters
+            {
+                ValidIssuers = issuers,
+                ValidateLifetime = false,
+                IssuerValidator = CustomIssuerValidator,
+                IssuerValidatorUsingConfiguration = null
+            };
+            try
+            {
+                var handler = new JwtSecurityTokenHandler().ValidateToken(token, tokenParams, out jwtoken);
+            }
+            catch (Exception ex)
+            { Console.WriteLine(ex); }
+        }
+
+        public static string CustomIssuerValidator(string issuer, SecurityToken securityToken, TokenValidationParameters validationParameters)
+        {
+            ;
+
+            return String.Empty;
         }
     }
 }
